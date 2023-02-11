3eca7e92da30d453bc14e2cb89a15b943ffcf20b
nsolanoe <nsolano050@gmail.com>
2023-02-11 16:49:13 -0500

    cambios 1

---
+++ deberes/deber 1/ejercicio_2.py
@@ -0,0 +1,11 @@
+#Ingresar nombre y apellido e imprimirlo al revés
+
+
+nombre = input("Por favor ingrese su nombre: ")
+apellido = input("Por favor ingrese su apellido: ")
+
+nombre_reves = nombre[::-1]
+apellido_reves = apellido[::-1]
+
+print("Su nombre y apellido al revés es:", nombre_reves, apellido_reves)
+
